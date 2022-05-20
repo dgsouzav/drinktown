0d8da5f01888a8cf16a60003d3e315a3d6ba1676
dgsouzav <diogouba11@gmail.com>
2022-05-20 17:41:12 -0300

    delete project

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./bin/index.js');
-const keys = require('./bin/keys.js');
-const connection = require('./src/middleware/connection.js');
-
-app.listen(keys.server.port, () => {
-    connection.connect();
-    console.log('Funcionando');
-});
